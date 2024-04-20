dfd1700f5528d31c2a13055dc0d21747df1cc55d
Harshit Ruwali <harshitruwali@outlook.com>
2024-04-20 18:43:22 +0530

    working on the form submission lambda

--- server/form_submission/app.py
+++ server/form_submission/app.py
@@ -1,10 +1,37 @@
 import traceback
+from datetime import datetime
+from connections import mongo_client
+
+mongo_obj = mongo_client()
 
 
 def handler(event, context):
     try:
         print("handler invoked")
         body = event["body"]
-
+        if "email" in body:
+            print("have the email to send the mail to")
+            # mongo insert
+            date_to_send = body["date_to_send"]
+            db = mongo_obj["future_note_emails"]
+            collection = db[date_to_send]
+            _insert_obj = {
+                "date": date_to_send,
+                "email": body["email"],
+                "content": body["content"],
+                "insert_time": datetime.now(),
+            }
+            collection.insert_one(_insert_obj)
+            return {
+                "body": "inserted successfully",
+                "statusCode": 200,
+            }
+        else:
+            return {
+                "body": "email is not present in the body",
+                "statusCode": 404,
+            }
     except Exception:
         print(f"Error in the handler execution : {traceback.format_exc()}")
+    finally:
+        mongo_obj.close()
